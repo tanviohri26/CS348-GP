a0a99877e8b591352d435ac7dd3c9b7e5abcae8f
Maanav Rajesh <65039621+maanav13@users.noreply.github.com>
2022-11-12 13:07:37 -0500

    Switch to MongoDB (#6)

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-// server/index.js
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const port = 3200;
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get("/test", (req, res) => {
-  res.send({ data: "Server says hi!" });
-});
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
