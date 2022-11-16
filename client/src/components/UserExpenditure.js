39f9bb9092eca494423f961ed233163f8ff6a15a
Maanav Rajesh <maanavrajesh13@gmail.com>
2022-11-15 20:56:43 -0500

    Fix react routes

---
+++ client/src/components/UserExpenditure.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class UserExpenditure extends Component {
+    render() {
+        return (
+        <div>
+            You are in UserExpenditure.
+        </div>
+        )
+    }
+}
