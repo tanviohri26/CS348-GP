a8bdbd10e84cf981a94aecd2d6756069c3aadd81
TANVI OHRI <114536381+tanviohri26@users.noreply.github.com>
2022-11-24 15:02:17 -0500

    Create R10.sql

---
+++ queries/R10.sql
@@ -0,0 +1,13 @@
+\connect cs348_gp47
+
+INSERT into Accounts 
+values (83873,63762, '100.34', 'Chequing');
+
+INSERT into Accounts 
+values (88834,93832, '100.34', 'Saving');
+
+INSERT into Accounts 
+values (53637,74636, '100.34', 'Credit');
+
+DELETE from Accounts 
+where AccountID = 53637;
